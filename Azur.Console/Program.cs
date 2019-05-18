c3919e4690b183e7476524dc9eea0691bbe4c3c6
fwahyudianto06 <fwahyudi06@gmail.com>
2019-05-18 21:46:36 +0700

    Initial Project

---
+++ Azur.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+#region Trademark
+
+// This software, all associated documentation, and all copies are CONFIDENTIAL INFORMATION of Kalpawreska Teknologi Indonesia 
+// http://www.fwahyudianto.id
+// ® Wahyudianto, Fajar
+// Email 	: fwahyudi06@gmail.com 
+
+#endregion
+
+namespace Azur.Consol
+{
+    class Program
+    {
+        #region Main Method
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Microsoft Azure - Blob Storage For .NET");
+            Console.ReadKey();
+        }
+
+        #endregion // end of MyRegion 
+    }
+}
---
+++ Azur.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Storage Blob")]
+[assembly: AssemblyDescription("Azur - Storage Blob")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kalpawreska Teknologi Indonesia")]
+[assembly: AssemblyProduct("Azur.Consol")]
+[assembly: AssemblyCopyright("© Wahyudianto, Fajar 2019")]
+[assembly: AssemblyTrademark("Kalpawreska Teknologi Indonesia")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("81a3a107-72f6-4611-aa86-216ed0a3c82e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//  A   :   Additions or Changes to the Module
+//  B   :   Additions or Changes to the Menu or User Interface
+//  C   :   Additions or Changes to functions that do not change the Menu or User Interface
+//  D   :   Bug Fixing
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
+
+/*
+ *  0.0.0.1    :   Initial Project
+ *  
+ */
