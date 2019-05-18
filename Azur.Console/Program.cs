a7177e70b98c5bc4a339a261f124c2286986958d
fwahyudianto06 <fwahyudi06@gmail.com>
2019-05-18 22:28:35 +0700

    Add References and ConnectionString

--- Azur.Console/Program.cs
+++ Azur.Console/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.WindowsAzure.Storage;
+using System.Threading.Tasks;
 
 #region Trademark
 
@@ -11,16 +13,73 @@
 
 namespace Azur.Consol
 {
-    class Program
+    public static class Program
     {
         #region Main Method
 
         static void Main(string[] args)
         {
             Console.WriteLine("Microsoft Azure - Blob Storage For .NET");
-            Console.ReadKey();
+            Console.WriteLine();
+
+            Execute();
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to exit Azur Application!");
+            Console.ReadLine();
         }
 
-        #endregion // end of MyRegion 
+        #endregion // end of Main Method
+
+
+        #region Method
+
+        private static void Execute()
+        {
+            //  Microsoft Azure Storage Account
+            CloudStorageAccount oCloudStorageAccount = null;
+
+            /* 
+             * Retrieve the connection string for use with the application. 
+             * The storage connection string is stored in an environment variable on the machine running the application called "storageconnectionstring".
+             */
+            string strStorageConnectionString = Environment.GetEnvironmentVariable("storageconnectionstring");
+
+            //  Check whether the connection string can be parsed.
+            if (CloudStorageAccount.TryParse(strStorageConnectionString, out oCloudStorageAccount))
+            {
+                try
+                {
+                    Console.WriteLine("Azure - Storage Account is \"Connected\" !");
+                }
+                catch (Exception oException)
+                {
+                    Console.WriteLine("Error returned from the service : {0}.", oException.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine(
+                    "A connection string has not been defined in the system environment variables. " +
+                    "Add a environment variable named \"storageconnectionstring\" with your storage " +
+                    "connection string as a value.");
+            }
+        }
+
+        #endregion // end of Method         
     }
 }
+
+
+#region Notes
+
+/// <summary>
+/// Microsoft Azure - Blob Storage For .NET 
+/// Demonstrate how to upload, list, download, and delete blobs.
+/// 
+///     <source>
+///     - https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-dotnet?tabs=windows
+///     </source>
+/// </summary>
+
+#endregion // end of Notes   
